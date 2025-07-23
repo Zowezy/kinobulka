7907772f1924b35e6a07d02b33f67f0d2a22e7e6
Denis <zowezy@yandex.ru>
2025-07-23 19:22:34 +0400

    fix redirect in params pages

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import Popular from './components/pages/Popular.vue'
 import Single from './components/pages/Single.vue'
 
 const router = createRouter({
-    history:createWebHistory(process.env.BASE_URL),
+    history:createWebHistory(import.meta.env.BASE_URL),
     routes:[
     {
         name:'Search',
